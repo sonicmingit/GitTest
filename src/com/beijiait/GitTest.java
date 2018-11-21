b5703766e7c6de4db080606e27b7afd33f75a980
Sonic@XPS <sonicming@gmail.com>
2018-11-21 20:52:31 +0800

    解决错误

---
+++ src/com/beijiait/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.beijiait;
+
+/**
+ * @author Sonic
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String str = "≤‚ ‘≥ÂÕª";
+		System.out.println(str);
+	}
+
+}
--- src/com/beijiait/Ping.java
+++ src/com/beijiait/Ping.java
@@ -3,18 +3,11 @@ package com.beijiait;
 import java.io.IOException;
 import java.net.InetAddress;
 
-/**
- * 一个ping的方法
- * @author haoyiming  
- * @date 2017年9月12日 下午5:05:39
- */
+
 public class Ping {
-	/**
-	 * ping一个网址
-	 * @param args 啥都不是
-	 */
+
 	public static void main(String[] args) {
-		String host = "220.181.57.217";
+		java.lang.String host = "220.181.57.217";
 		int timeOut = 3000;
 		try {
 			boolean status = InetAddress.getByName(host).isReachable(timeOut);
